25b71b48abed95bebd7cfab4e7e56479986552ad
ignacio mendez <ignacio.mendez0000@gmail.com>
2022-04-10 18:56:40 -0300

    coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.4"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("org.jetbrains.kotlinx.kover") version "0.4.2"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.4.31"
 	kotlin("plugin.jpa") version "1.4.31"
