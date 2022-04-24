2073ea3be8a9ceb510203f5893ef41bc515c9dc8
Nicolas Martinez <martinez.nicolas0510@gmail.com>
2022-04-23 21:48:07 -0300

    Se termino de eliminar oferta del modelo

--- src/main/kotlin/ar/edu/unq/postinscripciones/persistence/OfertaRepository.kt
+++
@@ -1,8 +0,0 @@
-package ar.edu.unq.postinscripciones.persistence
-
-import ar.edu.unq.postinscripciones.model.Oferta
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface OfertaRepository: CrudRepository<Oferta, Long>
