60622d0af9eb8ff2b48fc3e08d5fd2fc0732fbab
Federico Sandoval <federico.sandoval@alu.unq.edu.ar>
2022-05-16 13:34:42 -0300

    :wrench: configuracion de cors

--- src/main/kotlin/ar/edu/unq/postinscripciones/webservice/security/SecurityConfig.kt
+++ src/main/kotlin/ar/edu/unq/postinscripciones/webservice/security/SecurityConfig.kt
@@ -1,18 +1,46 @@
 package ar.edu.unq.postinscripciones.webservice.security
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
+@Profile("!test")
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Value("\${unque.frontend.urls}")
+    private lateinit var allowedFrontends: List<String>
 
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests()
             .antMatchers("/**").permitAll()
             .and()
+            .cors().configurationSource(corsConfigurationSource())
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             .csrf().disable()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = allowedFrontends
+        configuration.allowedMethods = listOf("*")
+        configuration.allowCredentials = true
+        configuration.exposedHeaders = listOf("*")
+        configuration.allowedHeaders = listOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
